ce57881f56b02da75d7c017d5ee18224f855b16c
saranshtyagi <18saranshtyagi2004@gmail.com>
2026-05-24 13:04:34 +0530

    Add pypdf_loader.py for loading and processing PDF documents

---
+++ pypdf_loader.py
@@ -0,0 +1,24 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.2-1B-Instruct",
+    task="text-generation",
+    max_new_tokens=512,
+    temperature=0.7
+)
+
+model = ChatHuggingFace(llm = llm)
+
+loader = PyPDFLoader('Air Pollution numerical.pdf')
+
+docs = loader.load()
+
+print(docs[0].page_content)
+
+print(len(docs))
