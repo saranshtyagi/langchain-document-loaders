6293fb0a1e106822ae095840e4dd4196c97d33e8
saranshtyagi <18saranshtyagi2004@gmail.com>
2026-05-25 13:50:34 +0530

    Add directory_loader.py for loading PDF documents from a specified directory

---
+++ directory_loader.py
@@ -0,0 +1,19 @@
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
+
+loader = DirectoryLoader(
+    path='books', 
+    glob='*.pdf', 
+    loader_cls=PyPDFLoader
+)
+
+# docs = loader.load()
+
+docs = loader.lazy_load()
+
+# print(len(docs))
+
+# print(docs[0].page_content)
+# print(docs[0].metadata)
+
+for document in docs:
+    print(document.metadata)
